5e766cda27094c4428cd429c6b8826504e3f1b53
William Radi <williamradi@gmail.com>
2018-11-13 15:12:24 -0200

    Um pouco de Blade

---
+++ website/resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('title', 'About us')
+
+@section('content')
+    
+    <h1>About us</h1>
+
+@endsection
---
+++ website/resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title', 'Laracast')</title>
+</head>
+<body>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/about">About us</a></li>
+    </ul>
+
+    @yield('content')
+
+</body>
+</html>
