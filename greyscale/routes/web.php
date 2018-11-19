470b9a790940e60de4e41258cf42cc5e59e9c69a
William Radi <williamradi@gmail.com>
2018-11-19 17:24:58 -0200

    GS - início do BandsController

--- greyscale/routes/web.php
+++ greyscale/routes/web.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * 
+ * 7 métodos principais:
+ * 
+ * GET /projects            @(index)     - Mostrar todos
+ * GET /projects/create     @(create)    - Formulário para criação
+ * GET /projects/{id}       @(show)      - View de um projeto
+ * POST /projects           @(store)     - Faz a persistência no banco
+ * GET /projects/{id}/edit  @(edit)      - Formulário de edição
+ * PATCH /projects/{id}     @(update)    - Persiste a edição no banco
+ * DELETE /projects/{id}    @(destroy)   - Deleta do banco
+ * 
+ */
+
+Route::resource('bands', 'BandsController');
+
 Route::get('/', 'PageController@index');
 
 Route::post('/', 'SkillController@store');
